12eabf6bc9fae9e4f3fabffcfb0c852b5979a07c
jajendrapandeya <=>
2020-12-04 21:18:02 +0545

    Implemented feature to add custom images from gallery

--- app/src/main/java/com/codermonkeys/mymemory/CreateActivity.kt
+++ app/src/main/java/com/codermonkeys/mymemory/CreateActivity.kt
@@ -1,31 +1,143 @@
 package com.codermonkeys.mymemory
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.codermonkeys.mymemory.models.BoardSize
 import com.codermonkeys.mymemory.utils.EXTRA_BOARD_SIZE
+import com.codermonkeys.mymemory.utils.isPermissionGranted
+import com.codermonkeys.mymemory.utils.requestPermission
 
 class CreateActivity : AppCompatActivity() {
 
+    companion object {
+        private const val TAG = "CreateActivity"
+        private const val PICK_PHOTO_CODE = 123
+        private const val READ_EXTERNAL_PHOTOS_CODE = 345
+        private const val READ_PHOTOS_PERMISSION = android.Manifest.permission.READ_EXTERNAL_STORAGE
+    }
+
+    private lateinit var rvImagePicker: RecyclerView
+    private lateinit var etGameName: EditText
+    private lateinit var btnSave: Button
+
+    private lateinit var adapter: ImagePickerAdapter
     private lateinit var boardSize: BoardSize
     private var numImagesRequired = -1
+    private var chosenImagesUri = mutableListOf<Uri>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
 
+        rvImagePicker = findViewById(R.id.rvImagePicker)
+        etGameName = findViewById(R.id.etGameName)
+        btnSave = findViewById(R.id.btnSave)
+
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         boardSize = intent.getSerializableExtra(EXTRA_BOARD_SIZE) as BoardSize
         numImagesRequired = boardSize.getNumPairs()
         supportActionBar?.title = "Choose pics (0 / $numImagesRequired)"
+
+        adapter = ImagePickerAdapter(
+            this,
+            chosenImagesUri,
+            boardSize,
+            object : ImagePickerAdapter.ImageClickListener {
+                override fun OnPlaceHolderClicked() {
+                    if (isPermissionGranted(this@CreateActivity, READ_PHOTOS_PERMISSION)) {
+                        launchIntentForPhotos()
+                    } else {
+                        requestPermission(
+                            this@CreateActivity,
+                            READ_PHOTOS_PERMISSION,
+                            READ_EXTERNAL_PHOTOS_CODE
+                        )
+                    }
+
+                }
+
+            })
+        rvImagePicker.adapter = adapter
+        rvImagePicker.setHasFixedSize(true)
+        rvImagePicker.layoutManager = GridLayoutManager(this, boardSize.getWidth())
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == READ_EXTERNAL_PHOTOS_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                launchIntentForPhotos()
+            } else {
+                Toast.makeText(
+                    this,
+                    "In order to create a custom game, you need to provide access to your photos",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(item.itemId == android.R.id.home) {
+        if (item.itemId == android.R.id.home) {
             finish()
             return true
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode != PICK_PHOTO_CODE || resultCode != Activity.RESULT_OK || data == null) {
+            Log.w(
+                TAG,
+                "Did not get data back from the launched activity, user likely cancelled flow ",
+            )
+        }
+
+        val selectedUri = data?.data
+        val clipData = data?.clipData
+
+        if (clipData != null) {
+            Log.i(TAG, "clipdata numImages ${clipData.itemCount}: $clipData ")
+            for (i in 0 until clipData.itemCount) {
+                val clipItem = clipData.getItemAt(i)
+                if (chosenImagesUri.size < numImagesRequired) {
+                    chosenImagesUri.add(clipItem.uri)
+                }
+            }
+        } else if (selectedUri != null) {
+            Log.i(TAG, "data: $selectedUri ")
+            chosenImagesUri.add(selectedUri)
+        }
+        adapter.notifyDataSetChanged()
+        supportActionBar?.title = "Choose pics (${chosenImagesUri.size} / $numImagesRequired)"
+        btnSave.isEnabled = shouldEnableSaveButton()
+    }
+
+    private fun shouldEnableSaveButton(): Boolean {
+        //Check if we should enable the save button or not
+        return true
+    }
+
+    private fun launchIntentForPhotos() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+        startActivityForResult(Intent.createChooser(intent, "Pick images"), PICK_PHOTO_CODE)
     }
 }
---
+++ app/src/main/java/com/codermonkeys/mymemory/ImagePickerAdapter.kt
@@ -0,0 +1,62 @@
+package com.codermonkeys.mymemory
+
+import android.content.Context
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.codermonkeys.mymemory.models.BoardSize
+import kotlin.math.min
+
+class ImagePickerAdapter(
+    private val context: Context,
+    private val imageUris: List<Uri>,
+    private val boardSize: BoardSize,
+    private val imageClickListener: ImageClickListener
+) :
+    RecyclerView.Adapter<ImagePickerAdapter.ViewHolder>() {
+
+    interface ImageClickListener {
+        fun OnPlaceHolderClicked()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.card_image, parent, false)
+        val cardWidth = parent.width / boardSize.getWidth()
+        val cardHeight = parent.height / boardSize.getHeight()
+        val cardSideLength = min(cardWidth, cardHeight)
+        val layoutParams = view.findViewById<ImageView>(R.id.ivCustomImage).layoutParams
+        layoutParams.width = cardSideLength
+        layoutParams.height = cardSideLength
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        if (position < imageUris.size) {
+            holder.bind(imageUris[position])
+        } else {
+            holder.bind()
+        }
+    }
+
+    override fun getItemCount() = boardSize.getNumPairs()
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val ivCustomImage = itemView.findViewById<ImageView>(R.id.ivCustomImage)
+
+        fun bind(uri: Uri) {
+            ivCustomImage.setImageURI(uri)
+            ivCustomImage.setOnClickListener(null)
+        }
+
+        fun bind() {
+            ivCustomImage.setOnClickListener {
+                //launch intent for user to select photo
+                imageClickListener.OnPlaceHolderClicked()
+            }
+        }
+
+    }
+}
